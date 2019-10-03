43e0d5c978fc051484abaa6faa20680e3e3e4ec0
Jacob <JacobEvans.7@outlook.com>
2019-10-03 23:55:39 +0800

    Solve kata "Vowel Count"

---
+++ 7 kyu/Vowel Count/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn get_count(string: &str) -> usize {
+    string
+        .chars()
+        .filter(|c| ['a', 'e', 'i', 'o', 'u'].contains(c))
+        .count()
+}
+
+#[test]
+fn my_tests() {
+    assert_eq!(get_count("abracadabra"), 5);
+}
